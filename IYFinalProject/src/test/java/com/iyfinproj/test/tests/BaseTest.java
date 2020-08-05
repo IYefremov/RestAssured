0da444f31ca6af46c422d57c67895d4a1260254b
IvanYefremov <iyefremov@yahoo.com>
2020-08-05 10:36:34 +0300

    FinalProjectIY

--- IYFinalProject/src/test/java/com/iyfinproj/test/tests/BaseTest.java
+++ IYFinalProject/src/test/java/com/iyfinproj/test/tests/BaseTest.java
@@ -1,12 +1,25 @@
 package com.iyfinproj.test.tests;
 
 import com.iyfinproj.test.Driver;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 
+import static com.iyfinproj.test.Driver.getDriver;
+
 public class BaseTest {
-    @AfterMethod
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(5000); // Debug purpose only
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(ITestResult result) throws InterruptedException {
+        if (!result.isSuccess()) {
+            screenCapture();
+        }
         Driver.quit();
+    }
+
+    @Attachment(type = "image/png")
+    private byte[] screenCapture() {
+        return ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
